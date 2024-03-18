9b69fed1de414d85826a5a33ce396687e85de4ea
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-03-18 18:29:30 +0530

    Add files via upload
    "Create a sleek and user-friendly responsive navbar with ease! This repository provides customizable code for building a navigation bar that adjusts seamlessly across various screen sizes, ensuring a seamless browsing experience for your website visitors."

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSideBar(){
+    const sideBar = document.querySelector(".sidebar");
+    sideBar.style.display = 'flex';
+}
+
+function hideSideBar(){
+    const sideBar = document.querySelector(".sidebar");
+    sideBar.style.display = 'none';
+}
